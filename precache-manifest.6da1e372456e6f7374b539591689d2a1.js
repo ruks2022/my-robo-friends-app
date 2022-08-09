507db0d72f54b40e950a9a725f478b69b18c5242
ruks2022 <shaikhruksar2022@gmail.com>
2022-08-09 09:37:16 +0530

    Updates

---
+++ precache-manifest.6da1e372456e6f7374b539591689d2a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1aeb62115cbc100a3b1",
+    "url": "/Robo-Friends/static/css/main.1dc4f91c.chunk.css"
+  },
+  {
+    "revision": "c1aeb62115cbc100a3b1",
+    "url": "/Robo-Friends/static/js/main.4c5f0899.chunk.js"
+  },
+  {
+    "revision": "c7980f8b10c6ecf9aa29",
+    "url": "/Robo-Friends/static/js/runtime~main.a23543f7.js"
+  },
+  {
+    "revision": "fb8bbce2f16b100e2f62",
+    "url": "/Robo-Friends/static/css/2.ae7f9e04.chunk.css"
+  },
+  {
+    "revision": "fb8bbce2f16b100e2f62",
+    "url": "/Robo-Friends/static/js/2.cf92c1d9.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robo-Friends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "6c8c6ffdcbd366b50d2a83c1c5877c68",
+    "url": "/Robo-Friends/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Robo-Friends/precache-manifest.6da1e372456e6f7374b539591689d2a1.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Robo-Friends/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
